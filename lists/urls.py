e04cec3aa09ea94715aa80578231b1191cae0987
MagdaZawora <magda.zawora@gmail.com>
2018-01-18 13:54:31 +0100

    Correction part 2

--- lists/urls.py
+++ lists/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from lists import views
 
+
 urlpatterns = [
     url(r'^new$', views.new_list, name='new_list'),
     url(r'^(\d+)/$', views.view_list, name='view_list'),
 ]
+
+
