e273045c992e26f65aca401a46f35d9c76ebc02d
MagdaZawora <magda.zawora@gmail.com>
2018-01-28 08:20:44 +0100

    Urls refactored

--- lists/views.py
+++ lists/views.py
@@ -32,5 +32,5 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
-    return redirect('/lists/%d/' % (list_.id,))
+    return redirect('view_list', list_.id)
 
